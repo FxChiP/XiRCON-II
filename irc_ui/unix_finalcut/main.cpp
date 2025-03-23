8a628a06ab9b3cf90c5f0b177ed7d514591bce48
davygrvy <davygrvy@DESKTOP-9L7JFU4>
2025-03-23 00:17:38 -0700

    Initial ideas for FinalCut

--- irc_ui/unix_finalcut/main.cpp
+++ irc_ui/unix_finalcut/main.cpp
@@ -1,3 +1,6 @@
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 // FinalCut TUI widgets
 // https://github.com/gansm/finalcut/wiki/First-steps
@@ -7,6 +10,12 @@ using namespace finalcut;
 
 #define USE_TCL_STUBS
 #include "TclEventSystem.hpp"
+
+extern "C" Tcl_AppInitProc Tui_irc_Init;
+
+// Direct access to these is limited to Tcl's thread only.
+Tcl_Interp* globalInterp;
+const char* rcScript = "./xirconrc.tcl";
 
 
 void
@@ -21,10 +30,47 @@ XircPanic TCL_VARARGS_DEF(const char*, arg1)
     // todo
 }
 
+class TclUp : public TclAsyncJob
+{
+public:
+    TclUp() { TclEventSystem::Instance()->QueueJob(this); }
+    ~TclUp() {}
+    bool AsyncProc() { return false; }
+    void EventProc() {
+	// This is running in Tcl's thread.
+	// NOT the main application thread.
+	Tcl_SetPanicProc(XircPanic);
+	globalInterp = Tcl_CreateInterp();
+
+	//Tcl_SetVar(globalInterp, "tcl_rcFileName", rcScript, TCL_GLOBAL_ONLY);
+	//Tcl_SetVar(globalInterp, "tcl_interactive", "1", TCL_GLOBAL_ONLY);
+
+	if (Tcl_Init(globalInterp) != TCL_OK) {
+	    Tcl_Panic(Tcl_GetStringResult(globalInterp));
+	}
+	//Tcl_SourceRCFile(globalInterp);
+	if (Tui_irc_Init(globalInterp) != TCL_OK) {
+	    Tcl_Panic(Tcl_GetStringResult(globalInterp));
+	}
+	tclStubsPtr->tcl_StaticPackage(globalInterp, "IRC_UserInterface", Tui_irc_Init, Tui_irc_Init);
+
+	// Source some stuff.
+	Tcl_SetVar(globalInterp, "argc", "1", TCL_GLOBAL_ONLY);
+	Tcl_SetVar(globalInterp, "argv", "xircon.tcl", TCL_GLOBAL_ONLY);
+	Tcl_SetVar(globalInterp, "argv0", "tkcon.tcl", TCL_GLOBAL_ONLY);
+	if (Tcl_EvalFile(globalInterp, "tkcon.tcl") != TCL_OK) {
+	    Tcl_Panic(Tcl_GetStringResult(globalInterp));
+	}
+    }
+};
+
+
 auto main(int argc, char* argv[]) -> int
 {
     int result;
     TclEventSystem* Tcl;
+    std::condition_variable cv;
+    std::mutex mtx;
     FApplication app(argc, argv);
 
     // now crank-up Tcl in it's own thread.  Use this tcl .so if specified
@@ -40,9 +86,11 @@ auto main(int argc, char* argv[]) -> int
     result = app.exec();
 
     // delete the global interp
-    new TclDown;
+    new TclDown(cv);
 
-    // todo, needs a wait() here
+    // todo
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock);
 
     // close the notifier thread
     Tcl->Shutdown();
